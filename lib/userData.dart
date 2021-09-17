afef4dbc2ee91a0a3d50241787281145b21d8a71
Huseina Jamil <jamilhuseina@gmail.com>
2021-09-17 15:53:53 +0000

    final gallery app project

---
+++ lib/userData.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: file_names
+
+List userData = [
+  {
+    "userName": "Taiwo Beauty Jamil",
+    "userLocation": "3rd Ridge Estate",
+    "userProfileImage": "images/profile_1.jpg",
+    "userPost": "images/picture_3.jfif",
+    "userPostLikes": "407",
+  },
+  {
+    "userName": "Lovelina Quayson",
+    "userLocation": "U.C.C. Cape Coast",
+    "userProfileImage": "images/profile_4.jpg",
+    "userPost": "images/picture_2.jfif",
+    "userPostLikes": "277",
+  },
+  {
+    "userName": "Ibrahim Alhassan",
+    "userLocation": "Siwdu Estate",
+    "userProfileImage": "images/profile_2.jpg",
+    "userPost": "images/picture_6.jfif",
+    "userPostLikes": "368",
+  }
+];
